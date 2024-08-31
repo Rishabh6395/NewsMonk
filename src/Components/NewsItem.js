354d00121267a7c8c9044f858babd3fc6f15565d
Rishabh <rishabhbhatt956@gmail.com>
2024-08-31 12:51:11 +0530

    Added isAbove

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -12,7 +12,7 @@ export class NewsItem extends Component {
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
                 {/* <p class="card-text"><small class="text-muted">By {author? "Unknown": author } on {date}</small></p> */}
-                <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More.</a>
             </div>
           </div>
       </div>
