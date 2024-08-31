838a953073df709e9cdcf8b8ca1f99cf6f3ff77b
Rishabh <rishabhbhatt956@gmail.com>
2024-08-31 23:50:19 +0530

    new feature added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function newFeatureAdd(){
+  console.log("this is a new feat");
+  
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
